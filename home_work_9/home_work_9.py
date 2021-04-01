5f6273e63b75613015f5c512f8077b998a9c75a4
LipatoS <saharubaiko@gmail.com>
2021-04-01 21:44:32 +0300

    task/hm_9: add Built-in Functions

--- home_work_9/home_work_9.py
+++ home_work_9/home_work_9.py
@@ -170,3 +170,61 @@ print(b)
 print(c)
 print(a.issuperset(b))
 print(b.issubset(c))
+
+
+# all()
+# The all() function returns True if all elements in the given iterable are true.
+# If not, it returns False.
+a = {0, 1, 2, 3, 4, 5}
+b = {4, 5, 6, 7, 8}
+print(all(a))
+print(all(b))
+
+# any()
+# The any() function returns True if any element of an iterable is True.
+# If not, any() returns False.
+a = {0}
+print(any(a))
+b = {4, 6, 0, 8}
+print(any(b))
+
+# enumerate()
+# The enumerate() method adds counter to an iterable and returns it (the enumerate object).
+set_ = {"milk", "butter", "bread"}
+print(list(enumerate(set_)))
+print(list(enumerate(set_, 10)))
+
+# The len() function returns the number of items (length) in an object.
+# len()
+a = {1, 2, 3, 4, 5}
+b = {"v", "a", "o", "b", "w", "c"}
+print(len(a))
+print(len(b))
+
+# max()
+# The Python max() function returns the largest item in an iterable.
+# It can also be used to find the largest item between two or more parameters.
+a = {2, 0, 77}
+b = {"aa", "aaa", "aaaaa"}
+print(max(a))
+print(max(b))
+
+# min()
+# The Python min() function returns the smallest item in an iterable.
+# It can also be used to find the smallest item between two or more parameters.
+a = {2, 0, 77}
+b = {"aa", "aaa", "aaaaa"}
+print(min(a))
+print(min(b))
+
+# sum()
+# The sum() function adds the items of an iterable and returns the sum.
+a = {1, 2.2, 2.8, -4, 5}
+print(sum(a))
+
+# sorted()
+# The sorted() function returns a sorted list from the items in an iterable.
+a = {5, 88, 2, 35, 6}
+b = {"v", "a", "o", "b", "w", "c"}
+print(sorted(a))
+print(sorted(b))
