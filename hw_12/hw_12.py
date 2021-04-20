b9fcbc2e503ba9e109bf7f1db62718ef987d8a17
LipatoS <saharubaiko@gmail.com>
2021-04-20 23:42:02 +0300

    tast/hw_12: home work is done.

--- hw_12/hw_12.py
+++ hw_12/hw_12.py
@@ -1,51 +1,51 @@
 import json
 
 
-def make_dict():
-    with open("HW.json", 'r') as json_data:
-        dic_d = json.load(json_data)
-    print('Файл загружен.')
-    return dic_d
+def makes_dict():
+    with open("HW.json", 'r') as json_file:
+        dict_ = json.load(json_file)
+        print('Файл загружен')
+    return dict_
 
 
-def dict_sort_by_type(dic_d):
-    employee = dic_d['employee']
-    my_dict4 = {}
-    for person in employee:
-        first_name = person.get('firstName')
-        last_name = person.get('lastName')
-        fio = f"{first_name} {last_name}"
-        ints = []
-        strings = []
-        floats = []
-        nons = []
+def dict_sort_types(result_dict):
+    employee = result_dict['employee']
+    main_dict = {}
+    for j in employee:
+        first_name = j.get('firstName')
+        last_name = j.get('lastName')
+        total_name = f"{first_name} {last_name}"
+        int_ = []
+        string_ = []
+        float_ = []
+        none_ = []
         bool_ = []
-        for pers_key, pers_value in person.items():
-            if type(pers_value) is int:
-                ints.append(pers_value)
-            elif type(pers_value) is str:
-                strings.append(pers_value)
-            elif type(pers_value) is float:
-                floats.append(pers_value)
-            elif type(pers_value) is None:
-                nons.append(pers_value)
-            elif type(pers_value) is bool:
-                bool_.append(pers_value)
-        my_dict2 = {'int': ints, 'string': strings, 'float': floats, 'None': nons, 'bool': bool_}
-        my_dict3 = {fio: my_dict2}
-        my_dict4.update(my_dict3)
-    dic_d.update({'employee': my_dict4})
-    print('Словарь отсортирован по типам.')
-    return dic_d
+        for k, v in j.items():
+            if type(v) == int:
+                int_.append(k)
+            elif type(v) == str:
+                string_.append(k)
+            elif type(v) == float:
+                float_.append(k)
+            elif type(v) is None:
+                none_.append(k)
+            elif type(v) == bool:
+                bool_.append(k)
+        dict_types = {'int': int_, 'string': string_, 'float': float_, 'None': none_, 'bool': bool_}
+        dict_names = {total_name: dict_types}
+        main_dict.update(dict_names)
+        result_dict.update({'employee': main_dict})
+    print('Словарь отсортирован по типам')
+    return result_dict
 
 
-def save_sorted_dict(dic_d):
-    with open('HW_result.json', 'w') as file:
-        json.dump(dic_d, file, indent=1)
+def save_sorted_dict(result_dict):
+    with open('./HW_result.json', 'w') as file:
+        json.dump(result_dict, file, indent=1)
 
 
 def main():
-    save_sorted_dict(dict_sort_by_type(make_dict()))
+    save_sorted_dict(dict_sort_types(makes_dict()))
 
 
 main()
