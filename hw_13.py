ca4cbe313403f92e75086857fc1534dcb42f46c1
LipatoS <saharubaiko@gmail.com>
2021-04-29 21:08:14 +0300

    task/hw_13: error correction

--- hw_13.py
+++ hw_13.py
@@ -1,44 +1,33 @@
 import json
 
 
-def main():
-    def some_func(*args, **kwargs):
-        result = {}
-        list_ = []
+def main(*args, **kwargs):
+    result = {}
+    list_ = []
+    list_4 = []
+    kol = 0
+    for i in args[0]:
+        list_4.append(i)
+        if kol < int(len(kwargs) * 4 / len(kwargs)) - 1:
+            kol += 1
+            continue
+        list_.append(list_4)
         list_4 = []
         kol = 0
-        for i in args:
-            list_4.append(i)
-            if kol < int(len(args) / len(kwargs)) - 1:
-                kol += 1
-                continue
-            list_.append(list_4)
-            list_4 = []
-            kol = 0
 
-        index_ = 0
-        for k, v in kwargs.items():
-            result[v] = list_[index_]
-            index_ += 1
-        return result
+    index_ = 0
+    for k, v in kwargs.items():
+        result[v] = list_[index_]
+        index_ += 1
+    return result
 
-    def load_dict(some_dict, json_path):
-        try:
-            with open(json_path, "r")as les_:
-                data = json.load(les_)
-        except FileNotFoundError:
-            with open(json_path, "w")as les_:
-                json.dump(results, les_, indent=1)
-        else:
-            data.update(results)
-            with open(json_path, "w") as les_:
-                json.dump(data, les_, indent=1)
 
-    results = some_func(51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
-                        one="one", two="two", three="three", four="four", five="five")
+def load_dict(some_dict, json_path):
+    with open(json_path, "w")as les_:
+        json.dump(results, les_, indent=1)
 
+
+if __name__ == '__main__':
+    results = main((i for i in range(51, 72)),
+                   one="one", two="two", three="three", four="four", five="five")
     load_dict(results, json_path="result.json")
-
-
-if __name__ == "__main__":
-    main()
